9ff5fae9a5e06335a108cb85e8640ce403fcdd98
Jie Liu <91784583+Jie1466803079@users.noreply.github.com>
2026-02-09 17:23:17 +1100

    Change base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/axe-f-capital/', // GitHub Pages: https://<username>.github.io/axe-f-capital
+  base: '/', // GitHub Pages: https://<username>.github.io/axe-f-capital
   plugins: [react()],
 })
